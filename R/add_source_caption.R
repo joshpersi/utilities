fc277f6058f69184da1b36d99761313969f5afeb
Josh Persi <joshpersi@gmail.com>
2025-11-08 03:06:26 -0800

    Fix spacing around colon in caption

--- R/add_source_caption.R
+++ R/add_source_caption.R
@@ -1,6 +1,6 @@
 add_source_caption <- function(source) {
   caption <- grid::textGrob(
-    label = bquote(bold("Data") * ": " ~ .(source)),
+    label = bquote(bold("Data") * ":" ~ .(source)),
     x = grid::unit(0.25, "in"),
     y = grid::unit(0.03, "npc"),
     hjust = 0,
